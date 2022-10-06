e6ca7edbe2b2fa9fe2b44a04f9f614d8b28cd659
Carolina Carvalho <carolina.dossantos0806@gmail.com>
2022-10-06 20:17:59 -0300

    Código padrão

--- padrao.py
+++ padrao.py
@@ -2,13 +2,31 @@ import pygame
 
 pygame.init()
 
-# Tela
+# Screen
 screen_w = 900
 screen_h = 600
 SIZE = [screen_w, screen_h]
 screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("Padrão")
+pygame.display.set_caption("Default")
 
+def level1():
+    print("oi")
+    pass
+
+def level2():
+    print("oi2")
+    pass
+
+def level3():
+    print("oi3")
+    pass
+
+def level4():
+    print("oi4")
+    pass
+
+def main():
+    pass
 
 done = False
 while not done:
@@ -16,6 +34,7 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
 
 
     pygame.display.update()
